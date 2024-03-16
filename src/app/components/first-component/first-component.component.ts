ed630e7dd580e8899f2b0378ff23cb841ea79050
damaralucena <damara.ti.dev@gmail.com>
2024-03-16 19:03:24 -0300

    Interpolação dos dados

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrl: './first-component.component.css'
 })
-export class FirstComponentComponent {
+export class FirstComponentComponent implements OnInit{
+  
+  //Trabalhando com o TypeScript
+  name: string = 'Mateus';
+  age: number = 24;
+  job: string = 'Desenvolvedora de software';
+  hobbies: string[] = ["Estudar", "jogar bola", "ler"];
+  car = {
+    name: "Polo",
+    year: 2019
+  } 
+  
   constructor() { }
 
   ngOnInit(): void{
-    
+
   }
 }
